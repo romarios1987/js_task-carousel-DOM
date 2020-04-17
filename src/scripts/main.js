0312df42927509d603979ab94aaf524b450aa839
romarios1987 <romios047@gmail.com>
2020-04-17 10:17:19 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,43 @@
+
 'use strict';
+
+let slideIndex = 1;
+const previousButton = document.querySelector('.carousel__btn_prev');
+const nextButton = document.querySelector('.carousel__btn_next');
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+previousButton.addEventListener('click', () => {
+  plusSlides(-1);
+});
+
+nextButton.addEventListener('click', () => {
+  plusSlides(1);
+});
+
+function showSlides(n) {
+  const slides = document.querySelectorAll('.twitter-card');
+  const dots = document.querySelectorAll('.carousel__dot');
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('carousel__dot_active');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].classList.add('carousel__dot_active');
+}
